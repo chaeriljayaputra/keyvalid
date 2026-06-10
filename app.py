ff10dd0d72e3803bac60b59eae105d6039a87135
chaeriljayaputra <chaeriljayaputra09@gmail.com>
2026-06-10 20:30:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ DEFAULT_KEYS = {
         "exp": "2026-06-15",
         "created_at": "2024-01-01"
     },
-        "P01": {
+    "P01": {
         "name": "1Day User", 
         "exp": "2026-06-12",
         "created_at": "2024-01-01"
