d77465350ba97578d389c7bdead96b61ffc438fd
chaeriljayaputra <chaeriljayaputra09@gmail.com>
2026-06-10 22:47:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ DEFAULT_KEYS = {
     },
 
     "003": {
-        "name": "User Free Key",
+        "name": "User Free Keyy",
         "exp": "2026-06-11",
         "created_at": "2024-01-01",
         "max_devices": 1,
